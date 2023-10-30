934bfc46fc674ce0075504d8649750a0cd2cb533
WallissonNarciso <105887429+WallissonNarciso@users.noreply.github.com>
2023-10-30 12:22:47 -0300

    Create heroFor.js
    Adicionado o arquivo com for

---
+++ heroFor.js
@@ -0,0 +1,14 @@
+let nome = "Teste";
+let xp = 6005;
+let nivel = "";
+
+const limitesXP = [1000, 2000, 5000, 7000, 8000, 9000, 10000];
+const niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
+
+for (let i = 0; i < limitesXP.length; i++) {
+    if (xp >= limitesXP[i]) {
+        nivel = niveis[i];
+    }
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
