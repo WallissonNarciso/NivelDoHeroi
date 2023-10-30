02c41bd33dde1eb4ace6d2433d224bc9a3b5d0b0
WallissonNarciso <105887429+WallissonNarciso@users.noreply.github.com>
2023-10-30 12:28:08 -0300

    Update heroFor.js

--- heroFor.js
+++ heroFor.js
@@ -2,12 +2,13 @@ let nome = "Teste";
 let xp = 6005;
 let nivel = "";
 
-const limitesXP = [1000, 2000, 5000, 7000, 8000, 9000, 10000];
+const limitesXP = [1000, 2000, 5000, 7000, 8000, 9000, 10000, Number.POSITIVE_INFINITY];
 const niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
 
 for (let i = 0; i < limitesXP.length; i++) {
-    if (xp >= limitesXP[i]) {
+    if (xp < limitesXP[i]) {
         nivel = niveis[i];
+        break;
     }
 }
 
