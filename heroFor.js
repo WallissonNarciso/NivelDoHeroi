ced5cdbe51fc38c26ba882bc3202d565b9013e47
WallissonNarciso <105887429+WallissonNarciso@users.noreply.github.com>
2023-10-30 12:34:57 -0300

    Update heroFor.js
    Adicionando comentário sobre o Number.POSITIVE_INFINITY

--- heroFor.js
+++ heroFor.js
@@ -4,6 +4,8 @@ let xp = 6005;
 let nivel = "";
 
 // Arrays para armazenar os limites de XP e os nomes de níveis correspondentes
+// Usamos Number.POSITIVE_INFINITY no último elemento do array limitesXP para representar o limite superior do último nível ("Radiante").
+// Isso permite que qualquer valor de XP maior que o último limite seja automaticamente considerado como "Radiante", tornando o código flexível e tratando todos os casos.
 const limitesXP = [1000, 2000, 5000, 7000, 8000, 9000, 10000, Number.POSITIVE_INFINITY];
 const niveis = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"];
 
